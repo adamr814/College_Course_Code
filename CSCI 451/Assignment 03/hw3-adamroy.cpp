63da02fb0540848e5c4952b7c846f59eb796d6b4
adamr814 <adamr814@outlook.com>
2023-09-23 21:54:46 -0500

    9/23/23 Daily Commit

--- CSCI 451/Assignment 03/hw3-adamroy.cpp
+++ CSCI 451/Assignment 03/hw3-adamroy.cpp
@@ -8,16 +8,28 @@ HW3
 #include <pthread.h>
 #include <iostream>
 #include <fstream>
-#include <cstring>
-using namespace std;
+#include <string>
+#include <condition_variable>
+#include <mutex>
 
-//Declaring global variables
-#define NUM_THREADS 2
-#define outFileName "hw3.out"
-int inInt = 0;
+//Declaring global variable and essential program dependencies
+int globalValue;
+std::mutex m;
+std::condition_variable cv;
+bool mutexLock = false;
 
-void* threadFunc1(void* arg){
-
+void* threadFunc1(void* filename_ptr){
+    const char* filename = static_cast<const char*>(filename_ptr);
+    std::ifstream inputFile(filename);
+    
+    if(inputFile.is_open)){
+        while(true){
+            int data;
+            if(inputFile >> data){
+                std::unique_lock
+            }
+        }
+    }
 }
 
 void* threadFunc2(void* arg){
@@ -26,12 +38,16 @@ void* threadFunc2(void* arg){
 
 int main(){
 //opening the hw3.out and checking if its open
-ofstream outFile;
-outFile.open(outFileName, ios::out);
+std::string inFileName = "hw3.in";
+std::string outFileName = "hw3.out";
+std::ofstream outFile;
+
+outFile.open(outFileName, std::ios::out);
 if (!outFile.is_open()){
-    cerr << "Failed to open the output file." << endl;
+    std::cerr << "Failed to open the output file." << std::endl;
     return 1;
 }
+thread thread1(threadFunc1, inFileName);
 
 outFile.close();
 return 0;
