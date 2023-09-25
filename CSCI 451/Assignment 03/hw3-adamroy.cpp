ca7ce5881e790952cfb99fbd363d825c443ed4a1
adamr814 <adamr814@outlook.com>
2023-09-25 18:10:37 -0500

    Working version - Not Tested on Server

--- CSCI 451/Assignment 03/hw3-adamroy.cpp
+++ CSCI 451/Assignment 03/hw3-adamroy.cpp
@@ -8,8 +8,6 @@ HW3
 #include <pthread.h>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <condition_variable>
 #include <mutex>
 
 //Declaring global variable and essential program dependencies
@@ -17,72 +15,67 @@ int globalValue;
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 bool mutexLock = false;
-bool endOfFile = false;
 
 void* threadFunc1(void* inFileName_ptr){
     const char* inFilename = static_cast<const char*>(inFileName_ptr);
     std::ifstream inputFile(inFilename);
     
-    if(inputFile.is_open()){
-        int data;
-        while(inputFile >> data){
-            pthread_mutex_lock(&m);
-            while(mutexLock){
-                pthread_cond_wait(&cv, &m);
-            }
+    if(!inputFile.is_open()){
+        std::cerr << "Error: Unable to open file " << inFilename << std::endl;
+        return nullptr;}
+        
+    int data;
+    while(inputFile >> data){
+        pthread_mutex_lock(&m);
+        while(mutexLock){
+            pthread_cond_wait(&cv, &m);}
+
             globalValue = data;
             mutexLock = true;
             pthread_mutex_unlock(&m);
-            pthread_cond_signal(&cv);
-        }
-        pthread_mutex_lock(&m);
-        endOfFile = true;
-        pthread_mutex_unlock(&m);
-        pthread_cond_signal(&cv);
-        inputFile.close();
-    } else {
-        std::cerr << "Error: Unable to open file " << inFilename << std::endl;
-    }
+            pthread_cond_signal(&cv);}
+
+    inputFile.close();
+
+    pthread_mutex_lock(&m); //P2
+    globalValue = -1;
+    mutexLock = true;
+    pthread_mutex_unlock(&m);
+    pthread_cond_signal(&cv);
     return nullptr;}
 
 void* threadFunc2(void* outFileName_ptr){
     const char* outFileName = static_cast<const char*>(outFileName_ptr);
     std::ofstream outFile(outFileName);
 
-    //this section checks the state of the mutex and waits until the input value is passed
-    if(outFile.is_open()){
-        while(true){
-            pthread_mutex_lock(&m);
-            while(!mutexLock){
-                pthread_cond_wait(&cv, &m);
-            }
-    /*
-    From here we need to check if the global value is odd or even and print to output accordingly
-    but first we need to make sure we aren't at the end of the file which causes an infinite loop (oops)
-    it was causing the program to never exit because the thread was never done and constantly spinning
-    */
-            if(mutexLock == true){
-                if(globalValue % 2 == 0){
-                    outFile << globalValue << std::endl;
-                    outFile << globalValue << std::endl;
-                    std::cout << "hw3.out <-- " << globalValue << std::endl; //console output for testing purposes
-                    std::cout << "hw3.out <-- " << globalValue << std::endl; //console output for testing purposes
-                }else{ 
-                    outFile << globalValue << std::endl;
-                    std::cout << "hw3.out <-- " << globalValue << std::endl; //console output for testing purposes
-                }
+    if(!outFile.is_open()){
+        std::cerr << "Error: Unable to open output file" << outFileName << std::endl;
+        return nullptr;}
+
+    while(true){
+        pthread_mutex_lock(&m);
+        while(!mutexLock){
+            pthread_cond_wait(&cv, &m);}
+
+        if(globalValue == -1){ //P1
+            pthread_mutex_unlock(&m);
+            break;}
+
+        if(globalValue % 2 == 0){
+            outFile << globalValue << std::endl;
+            outFile << globalValue << std::endl;
+            std::cout << "hw3.out <-- " << globalValue << std::endl;
+            std::cout << "hw3.out <-- " << globalValue << std::endl;}
+        else{ 
+            outFile << globalValue << std::endl;
+            std::cout << "hw3.out <-- " << globalValue << std::endl;}
 
             mutexLock = false;
-            pthread_cond_signal(&cv);}
-            else if (endOfFile){
-                pthread_mutex_unlock(&m);
-                break;}
             pthread_mutex_unlock(&m);
-        }
-        outFile.close();
-    } else {
-        std::cerr << "Error: Unable to open output file" << outFileName << std::endl;}
-        return nullptr;}
+            pthread_cond_signal(&cv);}
+
+    outFile.close();
+    return nullptr;}
 
 int main(){
 //opening the hw3.out and checking if its open
@@ -90,25 +83,39 @@ std::string inFileName = "hw3.in";
 std::string outFileName = "hw3.out";
 std::ofstream outFile;
 
-outFile.open(outFileName, std::ios::out);
-if (!outFile.is_open()){
-    std::cerr << "Error: Failed to open the output file." << std::endl;
-    return 1;
-}
-
 pthread_t thread1;
 pthread_t thread2;
 
 if(pthread_create(&thread1, nullptr, threadFunc1, (void*)inFileName.c_str()) != 0){
     std::cerr << "Error: Failed to create thread 1" << std::endl;
-    return 1;
-}
+    return 1;}
+
 if(pthread_create(&thread2, nullptr, threadFunc2, (void*)outFileName.c_str()) != 0){
     std::cerr << "Error: Failed to create thread 2" << std::endl;
-    return 1;
-}
+    return 1;}
 
 pthread_join(thread1, nullptr);
 pthread_join(thread2, nullptr);
 return 0;
 }
+
+/*
+Problems and solutions
+
+P1:
+this was the main problem earlier and I overlooked it, essentially when checking for the
+end of the file (non-recursively) the thread would never enter the case where it could finish
+its task therefore it was stuck in a loop looking for -1 which never came since thread 1 was
+done passing values to thread 2 so the thread could never exit and the main function could
+never return 0 exiting the program. - This was fixed by placing the check for -1 at the top
+of the while loop which meant that unless -1 was seen it would continue to loop and search
+for data instead of just getting to the EOF loop with no -1 and jumping into the infinite state
+
+P2:
+This was another large problem, a version of the code exists where the program would execute
+properly but it would only output the second to last number and never the last number. essentially
+what was happening is before it was sending the last number to the T2 it would instead send the EOF
+and never print the last number. - This was solved by moving the set global to -1 to the very end
+of the thread function making sure it was the last thing the function did after it had no more data
+instead of breaking out of the loop if it found a EOF.
+*/
